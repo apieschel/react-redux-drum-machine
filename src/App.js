20389bd0dcb9c48aae3c5896325708d1dbea2c7d
apieschel <apieschel@gmail.com>
2018-09-18 14:08:03 -0500

    added HTML audio elements

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import { Provider, connect } from 'react-redux';
-import { createStore, combineReducers, applyMiddleware } from 'redux';
-import logo from './logo.svg';
+import { createStore } from 'redux';
 import './App.css';
 import './drum-machine.css';
 
@@ -55,16 +54,43 @@ class App extends Component {
     return (
       <div id="display">
         <h1>Bang the Drum</h1>
-        <div class="pad-container col-12">
-          <div id="kick" class="drum-pad">Q</div>
-          <div id ="ride" class="drum-pad">W</div>
-          <div id="snare" class="drum-pad">E</div>
-          <div id="crash" class="drum-pad">A</div>
-          <div id="lowTom" class="drum-pad">S</div>
-          <div id="highTom" class="drum-pad">D</div>
-          <div id="clap" class="drum-pad">Z</div>
-          <div id="cowBell" class="drum-pad">X</div>
-          <div id="triangle" class="drum-pad">C</div>
+        <div className="pad-container col-12">
+          <div id="kick" className="drum-pad">
+						<audio src="https://s3.amazonaws.com/freecodecamp/drums/Heater-1.mp3" className="clip" id="Q"></audio>
+						Q
+					</div>
+          <div id ="ride" className="drum-pad">
+						<audio src="https://s3.amazonaws.com/freecodecamp/drums/Heater-1.mp3" className="clip" id="W"></audio>
+						W
+					</div>
+          <div id="snare" className="drum-pad">
+						<audio src="https://s3.amazonaws.com/freecodecamp/drums/Heater-1.mp3" className="clip" id="E"></audio>
+						E
+					</div>
+          <div id="crash" className="drum-pad">
+						<audio src="https://s3.amazonaws.com/freecodecamp/drums/Heater-1.mp3" className="clip" id="A"></audio>
+						A
+					</div>
+          <div id="lowTom" className="drum-pad">
+						<audio src="https://s3.amazonaws.com/freecodecamp/drums/Heater-1.mp3" className="clip" id="S"></audio>
+						S
+					</div>
+          <div id="highTom" className="drum-pad">
+						<audio src="https://s3.amazonaws.com/freecodecamp/drums/Heater-1.mp3" className="clip" id="D"></audio>
+						D
+					</div>
+          <div id="clap" className="drum-pad">
+						<audio src="https://s3.amazonaws.com/freecodecamp/drums/Heater-1.mp3" className="clip" id="Z"></audio>
+						Z
+					</div>
+          <div id="cowBell" className="drum-pad">
+						<audio src="https://s3.amazonaws.com/freecodecamp/drums/Heater-1.mp3" className="clip" id="X"></audio>
+						X
+					</div>
+          <div id="triangle" className="drum-pad">
+						<audio src="https://s3.amazonaws.com/freecodecamp/drums/Heater-1.mp3" className="clip" id="C"></audio>
+						C
+				</div>
         </div>
       </div>
     );
@@ -88,12 +114,9 @@ const NuProvider = Provider;
 const NuConnect = connect;
 
 // define the Container component here:
-const Container = connect(mapStateToProps, mapDispatchToProps)(App);
+const Container = NuConnect(mapStateToProps, mapDispatchToProps)(App);
 
-class AppWrapper extends React.Component {
-  constructor(props) {
-    super(props);
-  }
+class AppWrapper extends Component {
   render() {
     // complete the return statement:
     return (
@@ -104,4 +127,4 @@ class AppWrapper extends React.Component {
   }
 };
 
-export default App;
+export default AppWrapper;
